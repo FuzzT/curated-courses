32e9674c19153175ecfe72119c5b1a17a341f85f
VishnuKumar750 <vk9027024@gmail.com>
2025-02-17 15:33:20 +0530

    hero section with navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,69 @@
-import { ArrowPathIcon, CloudArrowUpIcon, LockClosedIcon } from '@heroicons/react/20/solid'
+import { AdjustmentsVerticalIcon, Bars2Icon, ChevronRightIcon } from '@heroicons/react/24/solid';
+import { useState } from 'react';
 
-const features = [
-  {
-    name: 'Push to deploy',
-    description: 'Lorem ipsum dolor sit amet consect adipiscing elit. Donec mollis nulla eget massa bibendum.',
-    icon: CloudArrowUpIcon,
-  },
-  {
-    name: 'SSL certificates',
-    description: 'Pellentesque eu mi vel lectus aliquam bibendum. Nullam consectetur ornare imperdiet.',
-    icon: LockClosedIcon,
-  },
-  {
-    name: 'Simple queues',
-    description: 'Sed tristique neque nec massa tincidunt, sit amet facilisis lectus vulputate. Fusce pulvinar.',
-    icon: ArrowPathIcon,
-  },
-]
 
 export default function FeatureCards() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setMenuOpen(menuOpen => !menuOpen);
+  }
+
   return (
-    <div className="bg-white py-24 sm:py-32">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl lg:text-center">
-          <h2 className="text-base font-semibold leading-7 text-indigo-600">Everything you need</h2>
-          <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            No server? No problem.
-          </p>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis.
-          </p>
-        </div>
-        <div className="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
-          <dl className="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-3 lg:gap-y-16">
-            {features.map((feature) => (
-              <div key={feature.name} className="relative pl-16">
-                <dt className="text-base font-semibold leading-7 text-gray-900">
-                  <div className="absolute left-0 top-0 flex h-12 w-12 items-center justify-center rounded-lg bg-indigo-600">
-                    <feature.icon className="h-6 w-6 text-white" aria-hidden="true" />
+    <>
+      <section className="p-2 antialiased">
+        <div className="bg-gradient-to-br from-amber-200 via-pink-300 to-fuchsia-600 rounded-3xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:max-w-7xl">
+            {/* navbar */}
+            <div className='pt-12 sm:pt-16'>
+              <nav className='flex items-center justify-between'>
+
+                <div className='flex items-center gap-x-2'>
+                  <AdjustmentsVerticalIcon className='w-6 h-6'/>
+                  <h1 className='text-xl font-medium'>Radiant</h1>
+                  <div className='bg-gray-600/35 rounded-full px-3 py-1/2 ml-2 hidden lg:block'>
+                    <p className='flex items-center justify-center gap-x-2 text-white text-base font-medium'>Radiant raises $100M Series A from Tailwind Ventures
+
+                      <ChevronRightIcon className='w-4 h-4' />
+                    </p>
                   </div>
-                  {feature.name}
-                </dt>
-                <dd className="mt-2 text-base leading-7 text-gray-600">{feature.description}</dd>
-                <div className="mt-6">
-                  <a 
-                    href="#" 
-                    className="text-sm font-semibold leading-6 text-indigo-600 hover:text-indigo-500 transition-colors"
-                  >
-                    Learn more <span aria-hidden="true">→</span>
-                  </a>
                 </div>
+
+
+                <Bars2Icon className='w-6 h-6 lg:hidden' onClick={handleMenuClick} />    
+                <div className='hidden lg:block'>
+                  <ul className='flex gap-x-4 font-medium'>
+                    <li>Pricing</li>
+                    <li>Company</li>
+                    <li>Blog</li>
+                    <li>Login</li>
+                  </ul>
+                </div>            
+              </nav>
+              {/* mobile menu */}
+              {menuOpen && (
+                <div className='pt-10 lg:hidden'>
+                  <ul className='space-y-4 text-base font-medium'>
+                    <li>Pricing</li>
+                    <li>Company</li>
+                    <li>Blog</li>
+                    <li>Login</li>
+                  </ul>
+                </div>
+              )}
+            </div>
+            <div className="pt-16 pb-24 sm:pt-24 sm:pb-32 md:pt-32 md:pb-48">
+              <h1 className="font-display text-6xl/[0.9] font-medium tracking-tight text-gray-900 sm:text-8xl/[0.8] md:text-9xl/[0.8] text-balance">Close every deal.</h1>
+              <p className="mt-8 max-w-lg text-xl/7 font-medium text-gray-950/75 sm:text-2xl/8">Radiant helps you sell more by revealing sensitive information about your customers.</p>
+              <div className="mt-12 flex flex-col gap-x-6 gap-y-4 sm:flex-row">
+                <button className="inline-flex items-center justify-center px-4 py-1 bg-gray-950 text-white rounded-full border border-transparent shadow-md text-base font-medium whitespace-nowrap ">Get started</button>
+                <button className="inline-flex items-center justify-center px-4 py-1 bg-transparent border-transparent  rounded-full border shadow-md text-base font-medium whitespace-nowrap ">See pricing</button>
               </div>
-            ))}
-          </dl>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </section>
+
+    </>
   )
 }
