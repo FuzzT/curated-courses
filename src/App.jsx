2a086a6018552ce92b87a862c8eeea74bf109824
VishnuKumar750 <vk9027024@gmail.com>
2025-02-20 16:45:16 +0530

    blog section in home page and blog list page creation.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import Footer from "./component/Footer";
+import Footer from './component/Footer';
+import BlogList from './pages/BlogList';
 import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export default function FeatureCards() {
  
   return (
-    <>
-      <Home />
+    <Router>
 
-      {/* Footer Section */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/AllBlogs" element={<BlogList />} />
+      </Routes>
       <Footer />
+    </Router>
 
-    </>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,41 @@
 import React from 'react'
 import Hero from '../component/Hero'
 import CourseSec from '../component/CourseSec'
+import { ArrowUpRightIcon } from '@heroicons/react/24/solid'
+import BlogCard from '../component/BlogCard'
+import { Link } from 'react-router-dom'
+import { blogPosts } from '../../data'
 
 const Home = () => {
+  
+  
+  
+
   return (
     <div className='w-full h-full'>
       <Hero />
       <CourseSec />
+      {/* blog */}
+      <section className='container mx-auto w-full min-h-[50vh] pt-12 mb-24  px-6 md:px-2'>
+        <div className='flex  items-center justify-between pb-16 '>
+          <h1 className='text-5xl/[0.9] font-medium '>Blog</h1>
+          <Link to={'/AllBlogs'}>
+            <div className='relative flex items-center justify-center'>
+              <div className='blob1 w-16 h-16'></div>
+              <div className='blob2 w-15 h-15'></div>
+              <ArrowUpRightIcon className='w-8 h-8 cursor-pointer ' />
+            </div>
+          </Link>
+        </div>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-4 gap-8'>
+        {blogPosts.map((post, index) => (
+          <BlogCard key={index} {...post} />
+        ))}
+        </div>
+      </section>
+
     </div>
+
   )
 }
 
